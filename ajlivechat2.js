25d8cf695a2b4717381127b918c76bb988ae3a23
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2025-11-05 23:08:38 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -585,7 +585,7 @@ document.addEventListener("DOMContentLoaded", function () {
     controlsOverlay.id = "video-controls-overlay";
     Object.assign(controlsOverlay.style, {
       position: "fixed",
-      bottom: "520px",
+      bottom: "20px",
       left: "50%",
       transform: "translateX(-50%)",
       zIndex: "9999",
@@ -650,4 +650,5 @@ document.addEventListener("DOMContentLoaded", function () {
     activationZone.addEventListener("touchstart", showControls);
   }, 300);
 });
+
 
