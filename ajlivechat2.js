1d3a411c93c47cc2456520dfc6b5a2311d5ceb24
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2026-04-05 00:43:57 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -612,7 +612,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const bgVideos = [
     document.getElementById("bg-video-1"),
-    document.getElementById("bg-video-2")
+    document.getElementById("bg-video-2"),
+    document.getElementById("bg-video-3")
     
 ];
 
