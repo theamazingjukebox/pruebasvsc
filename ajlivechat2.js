36444ec3f672e6833a81d112e79132556cebea49
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2026-08-02 03:12:30 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -840,7 +840,7 @@ document.addEventListener("DOMContentLoaded", () => {
         .album-art-container img {
           width: 95px; height: 95px; border-radius: 12px;
           border: 0px solid #87ffff; /* Borde Cian Orbitron */
-          box-shadow: 0 0 12px rgba(73, 255, 246, 0.2);
+          box-shadow: 0 0 0px rgba(73, 255, 246, 0.2);
           object-fit: cover; background-color: #12161e;
         }
         .track-info {
