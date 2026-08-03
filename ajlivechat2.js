2c6dc2fc52280668a548cd3da8cd6b82a9d057ff
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2026-08-03 01:12:16 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -839,13 +839,13 @@ document.addEventListener("DOMContentLoaded", () => {
           display: flex; width: 100%; align-items: center; gap: 16px; margin-top: -10px; margin-left: -10px;
         }
         .album-art-container img {
-          width: 145px; height: 145px; border-radius: 39px;
+          width: 155px; height: 155px; border-radius: 39px;
           border: 0px solid #87ffff; /* Borde Cian Orbitron */
           box-shadow: 0 0 opx rgba(73, 255, 246, 0.2);
           object-fit: cover; background-color: #12161e;
         }
         .track-info {
-          display: flex; flex-direction: column; flex: 1; overflow: hidden; margin-left: 55px; margin-top: 40px;
+          display: flex; flex-direction: column; flex: 1; overflow: hidden; margin-left: 65px; margin-top: 40px;
         }
         #mini-track-title {
           color: #87ffff; /* Color de tu texto principal */
@@ -863,7 +863,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         .widget-controls {
           display: flex; justify-content: center; align-items: center; gap: 10px;
-          width: 100%; margin-top: -55px; margin-left: 120px;
+          width: 100%; margin-top: -65px; margin-left: 120px;
         }
         
         /* ESTILO BURBUJA PARA TODOS LOS BOTONES */
@@ -878,8 +878,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }
         .btn-main { 
           font-size: 20px; 
-          color: #87ffff; /* Destaca el Play/Pause en cian */
+          color: #d83ca4; /* Destaca el Play/Pause en cian */
           width: 30px; 
+        }
+        .btn-main:hover { 
+          color: #d83ca4; 
+          text-shadow: 0 0 12px #d83ca4; 
         }
         .mini-footer { width: 100%; text-align: center; margin-top: 2px; }
         .mini-footer .tagline {
