5caf9620ac08310c018f129f6a925c34c7d2fe89
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2026-08-04 14:14:12 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -889,12 +889,12 @@ document.addEventListener("DOMContentLoaded", () => {
           align-items: center; 
           
           /* 1. Definimos un borde transparente del grosor que quieras (ej. 2px o 3px) */
-          border: 2px solid transparent;
+          border: 0px solid transparent;
           border-radius: 24px; /* ¡Ahora el navegador SÍ respetará las esquinas! */
 
           /* 2. EL TRUCO MAESTRO: Doble fondo separado por comas */
           /* El primer fondo es tu color oscuro (recortado al contenido). El segundo es el degradado que actuará como borde */
-          background-image: url("mini-footer9.svg"), 
+          background-image: url("mini-footer9.1.svg"), 
                             linear-gradient(rgba(18, 22, 30, 0.90), rgba(18, 22, 30, 0.90)), 
                             linear-gradient(135deg, #d83ca4, #87ffff);
           
@@ -1359,7 +1359,7 @@ document.addEventListener("DOMContentLoaded", () => {
   preloadJukeboxCursor.src = 'neoncursor4.png';
 
     const preloadJukeboxBgSvg = new Image();
-  preloadJukeboxBgSvg.src = 'mini-footer9.svg';
+  preloadJukeboxBgSvg.src = 'mini-footer9.1.svg';
 
     
 })();
