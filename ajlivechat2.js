10657708a3b14de494ef35e247bc46d5875f8550
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2026-08-04 13:05:57 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -895,7 +895,7 @@ document.addEventListener("DOMContentLoaded", () => {
           /* 2. EL TRUCO MAESTRO: Doble fondo separado por comas */
           /* El primer fondo es tu color oscuro (recortado al contenido). El segundo es el degradado que actuará como borde */
           background-image: url("mini-footer9.svg"), 
-                            linear-gradient(rgba(18, 22, 30, 0.90), rgba(18, 22, 30, 0.90)), 
+                            linear-gradient(rgba(18, 22, 30, 0.80), rgba(18, 22, 30, 0.80)), 
                             linear-gradient(135deg, #d83ca4, #87ffff);
           
           /* Indicamos cómo se debe recortar cada capa de fondo */
