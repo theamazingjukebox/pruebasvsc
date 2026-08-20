a74897156ee9205613b99f7fd852cdb92ec15953
theamazingjukebox <142849057+theamazingjukebox@users.noreply.github.com>
2026-08-20 10:38:03 -0600

    Update ajlivechat2.js

--- ajlivechat2.js
+++ ajlivechat2.js
@@ -196,7 +196,7 @@ const messagesContainer = document.getElementById("messages");
 // NO se guarda en Firebase.
 const welcomeMessage = `
     <li class="system chat-welcome">
-        Welcome to The Amazing Jukebox LIVE CHAT.<br>
+        ——Welcome to The Amazing Jukebox LIVE CHAT——<br>
         Please be polite, enjoy the music and feel free to have fun.<br>
         &blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;&blk12;
     </li>
